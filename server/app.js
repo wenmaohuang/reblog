28733cb46842fd2f085df6bab111e1367c3665c2
unknown <wenmaohuang@hotmail.com>
2020-09-01 03:05:54 +0800

    '1'

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 // app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.static(path.join(__dirname, 'public')));
 

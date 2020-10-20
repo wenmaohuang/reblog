3a6cffb6c3ff302d4e4294dbc341caa8066df162
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-20 20:40:13 +0800

    '1'

---
+++ client/build/precache-manifest.6ea0dd6ef3e6f51484516ffe710a556d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f24e2f90124ae6e6021349a0c015e2",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "ef85dacf8874dc29c193",
+    "url": "/blog/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "60bfc926aef2425bddac3954bfaf5298",
+    "url": "/blog/static/css/2.9c8c8187.chunk.css.gz"
+  },
+  {
+    "revision": "6bc892b9691a23efc05f",
+    "url": "/blog/static/css/main.39943da0.chunk.css"
+  },
+  {
+    "revision": "ef85dacf8874dc29c193",
+    "url": "/blog/static/js/2.88acebc7.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "/blog/static/js/2.88acebc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4da49f63d4e11d50b41247ff980ad64",
+    "url": "/blog/static/js/2.88acebc7.chunk.js.gz"
+  },
+  {
+    "revision": "6bc892b9691a23efc05f",
+    "url": "/blog/static/js/main.73e31502.chunk.js"
+  },
+  {
+    "revision": "83e062e7f388d90c2ffe2e270903eed6",
+    "url": "/blog/static/js/main.73e31502.chunk.js.gz"
+  },
+  {
+    "revision": "ca2e9f47cb68f982ead6",
+    "url": "/blog/static/js/runtime-main.4a7490d5.js"
+  },
+  {
+    "revision": "1636a10be61377b76eb1393e4deb4518",
+    "url": "/blog/static/media/2.1636a10b.jpg"
+  },
+  {
+    "revision": "073b67f9e682ed876b33034fd39cded9",
+    "url": "/blog/static/media/barbarahand-webfont.073b67f9.woff"
+  },
+  {
+    "revision": "380d64bac1b1ab5f4d608e62f5b7a726",
+    "url": "/blog/static/media/barbarahand-webfont.380d64ba.eot"
+  },
+  {
+    "revision": "76f50013c3d70ab6966d616b07c97b8f",
+    "url": "/blog/static/media/barbarahand-webfont.76f50013.svg"
+  },
+  {
+    "revision": "c60d9f240e71338c2987289df3eae1e8",
+    "url": "/blog/static/media/barbarahand-webfont.c60d9f24.woff2"
+  },
+  {
+    "revision": "fd080974ea15e1642c5d83a27d856bbb",
+    "url": "/blog/static/media/barbarahand-webfont.fd080974.ttf"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1515aba55ba3c78e9401eadce8058a66.js"
+  "/blog/precache-manifest.6ea0dd6ef3e6f51484516ffe710a556d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

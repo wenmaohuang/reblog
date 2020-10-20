083ca8a4a4a9feeda0e8dd81d8e3e04de1d16423
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-21 04:33:56 +0800

    '1'

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 
 router.use('/upload',require('./upload/index.js'))
-router.use('/article',require('./article/index.js'))
+router.use('/reblog/article',require('./article/index.js'))
 router.use('/daily',require('./daily/index.js'))
 router.use('/search',require('./search/index.js'))
 module.exports = router;

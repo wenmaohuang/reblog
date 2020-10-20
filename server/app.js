a32d4df2e04060dcb7e8544bbc80b4a96466b827
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-21 01:17:25 +0800

    '1'

--- server/app.js
+++ server/app.js
@@ -3,24 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-
-
 var mongoose = require('mongoose')
-
 var app = express();
-
-
-// mongoose.connect("mongodb://localhost:27017/blog",{ useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(()=>{console.log("数据库连接成功")})
-//     .catch(()=>{console.log("数据库连接失败")});
-
-
-// view engine setup
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +13,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    response.sendFile(path.resolve(__dirname, '../client/build/reblog', 'index.html'))
 })
 app.use((req, res, next) => {
     res.header({
@@ -37,7 +21,6 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Origin': req.headers.origin || '*',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
-
         // "Content-Security-Policy": "upgrade-insecure-requests"
         // 'Content-Type': 'application/json; charset=utf-8'
     });
@@ -47,42 +30,6 @@ app.use((req, res, next) => {
         next();
     }
 });
-
-
 app.use(require('./session/index.js'))
 app.use('/', require('./routes/index.js'))
-
-// const routesList = [
-//     {
-//         path: '/',
-//         name: 'admin',
-//     },
-//     {
-//         path: '',
-//         name: 'adminIndex',
-//     },
-//     {
-//         path: '/articleAdd',
-//                 name: 'Add',
-//     },
-//     {
-//         path: '/articleManager',
-//                 name: 'Manager',
-//     },
-//     {
-//         path: '/login',
-//         name: 'login',
-//     }
-// ]
-// routesList.forEach(item=>{
-// app.get(item.path,(req,res)=>{
-//     res.sendFile(require('path').join(__dirname,'../client/dist/index.html'))
-// })
-// })
-
-
-
-
-
-
 module.exports = app;
